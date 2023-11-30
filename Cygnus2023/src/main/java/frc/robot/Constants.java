798b93d2483fe29bba332bfa5ad52b3ea4e9d608
saramg02 <99089055+saramg02@users.noreply.github.com>
2023-11-29 19:12:40 -0600

    Add files via upload

---
+++ Cygnus2023/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+public final class Constants {
+
+  //tiempo que tiene la roborio como loop
+  public static final double kLooperDt = 0.02;
+  
+  //---------------------controles-----------------------//
+
+  //Puertos donde conectamos los controles
+  public static final int kChassisPort = 0;
+  public static final int kMecanismPort = 1;
+  //Tolerancia del stick
+  public static final double kStickTolerance = 0.15;
+
+  //---------------------controles-----------------------//
+
+  //---------------------chasis--------------------------//
+  public static final int kDriveMotor1 = 0;
+  public static final int kDriveMotor2 = 1;
+  public static final int kDriveMotor3 = 2;
+  public static final int kDriveMotor4 = 3;
+  //---------------------chasis--------------------------//
+}
